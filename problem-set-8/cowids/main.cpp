3d80cbea1403db7f65ccbb7368204a54388dbfd0
Kento Nishi <kento24gs@outlook.com>
2020-03-28 16:45:34 -0700

    Finished cowids

--- problem-set-8/cowids/main.cpp
+++ problem-set-8/cowids/main.cpp
@@ -40,44 +40,38 @@ SAMPLE OUTPUT:
 #define int64 long long
 using namespace std;
 
-vector<char> num;
+vector<int> num;
 
 void nextNum() {
-    int moveDigit = -1;
-    int digit = 0;
-    while (num.size() - digit > 0) {
-        if (moveDigit == -1 && (num[num.size() - 1 - digit] == '1' && (num.size() - digit == 1 || num[num.size() - 2 - digit] == '0'))) {
-            // remaining digits look like "01"
-            moveDigit = digit;
+    int splitDigit;
+    int numDigits = num.size();
+    for (int i = 0; i < numDigits; i++) {
+        if (numDigits - 2 - i < 0 ||
+            (num[numDigits - 1 - i] == 1 &&
+             num[numDigits - 2 - i] == 0)) {
+            splitDigit = numDigits - i;
+            break;
         }
-        digit++;
     }
-    vector<char> rightPart;
-    digit = 0;
-    for (int i = 0; i < moveDigit; i++) {
-        if (num[num.size() - 1 - digit] == '1') {
-            rightPart.push_back('1');
-        } else {
-            rightPart.push_back('0');
-        }
-        digit++;
-    }
-    vector<char> leftPart;
-    for (int i = 0; i < num.size() - digit; i++) {
-        leftPart.push_back(num[i]);
-    }
-    if (leftPart.size() >= 2) {
-        swap(leftPart[leftPart.size() - 1], leftPart[leftPart.size() - 2]);
+    if (splitDigit == 1) {
+        num.insert(num.begin() + 1, 0);
+        splitDigit++;
+        numDigits++;
     } else {
-        leftPart.push_back('0');
+        swap(num[splitDigit - 1], num[splitDigit - 2]);
     }
-    /*
-    cout << "Move digit: " << moveDigit << endl;
-    cout << "Right part: " << rightPart << endl;
-    cout << "Left part: " << leftPart << endl;
-    */
-    num = leftPart;
-    num.insert(num.end(), rightPart.begin(), rightPart.end());
+    int totalOnRight = 0;
+    for (int i = splitDigit; i < numDigits; i++) {
+        if (num[i] == 1) {
+            totalOnRight++;
+        }
+    }
+    for (int i = splitDigit; i < numDigits - totalOnRight; i++) {
+        num[i] = 0;
+    }
+    for (int i = numDigits - totalOnRight; i < numDigits; i++) {
+        num[i] = 1;
+    }
 }
 
 int main() {
@@ -92,14 +86,10 @@ int main() {
         return 0;
     }
     for (int64 i = 0; i < K; i++) {
-        num.push_back('1');
+        num.push_back(1);
     }
     for (int64 i = 0; i < N - 1; i++) {
         nextNum();
-        /*
-        cout << num << endl;
-        cout << "===================" << endl;
-        */
     }
     for (int i = 0; i < num.size(); i++) {
         cout << num[i];
